9b8af2e60576f8546982d3005b2d1c1756cf9c96
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-06-01 15:13:13 -0600

    feat: agregar link e ícono para regresar a página home

--- resources/views/backend/menus/navbar.blade.php
+++ resources/views/backend/menus/navbar.blade.php
@@ -8,7 +8,9 @@
 
     <ul class="navbar-nav">
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link" style="color: white">Sistema de Registro de Vehículos</a>
+            <a href="{{ route('slider.show') }}" target="frameprincipal" class="nav-link" style="color: white">
+                <i class="fas fa-home"></i>
+            </a>
         </li>
     </ul>
 
--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -1,6 +1,6 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="#" class="brand-link">
+    <a href="{{ route('slider.show') }}" target="frameprincipal" class="brand-link">
         <img src="{{ asset('images/Logo_carros.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
         <span class="brand-text font-weight" style="color: white">Dashboard</span>
     </a>
